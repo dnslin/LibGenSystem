6f540777364ed429008d83d418aad9183864dfb5
dnslin <1636988859@qq.com>
2021-12-11 20:55:53 +0800

    GateWay 模块修复bug

--- LibGenSystem-gateway/src/main/java/com/dnslin/gateway/service/ValidateCodeService.java
+++ LibGenSystem-gateway/src/main/java/com/dnslin/gateway/service/ValidateCodeService.java
@@ -1,23 +1,23 @@
 package com.dnslin.gateway.service;
 
 import java.io.IOException;
+
 import com.dnslin.common.core.exception.CaptchaException;
 import com.dnslin.common.core.web.domain.AjaxResult;
 
 /**
  * 验证码处理
- * 
+ *
  * @author DnsLin
  */
-public interface ValidateCodeService
-{
+public interface ValidateCodeService {
     /**
      * 生成验证码
      */
-    public AjaxResult createCapcha() throws IOException, CaptchaException;
+    AjaxResult createCapcha() throws IOException, CaptchaException;
 
     /**
      * 校验验证码
      */
-    public void checkCapcha(String key, String value) throws CaptchaException;
+    void checkCapcha(String key, String value) throws CaptchaException;
 }
